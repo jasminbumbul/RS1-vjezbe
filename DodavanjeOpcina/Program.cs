d8fbc8ef4d8f55f07ade9a262850fa82dc1aaea1
Jasmin Bumbul <jasmin.bumbul@edu.fit.ba>
2020-12-10 18:11:07 +0100

    popravljeno dodavanje općina

--- DodavanjeOpcina/Program.cs
+++ DodavanjeOpcina/Program.cs
@@ -1,7 +1,7 @@
 ﻿using RS1_vjezbe.EF;
 using RS1_vjezbe.EntityModels;
 using System;
-
+using System.Threading;
 
 namespace DodavanjeOpcina
 {
@@ -9,13 +9,18 @@ namespace DodavanjeOpcina
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Unesite naziv nove općine: ");
+            Console.WriteLine("Press ESC to stop");
 
-            var novaOpcina = new Opcina { NazivOpcine = Console.ReadLine() };
+            while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Unesite naziv nove općine: ");
 
-            MojDbContext dbContext = new MojDbContext();
-            dbContext.Add(novaOpcina);
-            dbContext.SaveChanges();
+                var novaOpcina = new Opcina { NazivOpcine = Console.ReadLine() };
+                MojDbContext dbContext = new MojDbContext();
+                dbContext.Add(novaOpcina);
+                dbContext.SaveChanges();
+            }
         }
     }
 }
